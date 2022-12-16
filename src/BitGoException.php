67d17b03b1ef42eab7508c2aac07f4baa641f759
Paulo R Lima <paulorlima333@gmail.com>
2022-12-16 16:17:26 -0300

    .

--- src/BitGoException.php
+++ src/BitGoException.php
@@ -1,21 +1,4 @@
 <?php
-/**
- * ======================================================================================================
- * File Name: BitGoException.php
- * ======================================================================================================
- * Author: HolluwaTosin360
- * ------------------------------------------------------------------------------------------------------
- * Portfolio: http://codecanyon.net/user/holluwatosin360
- * ------------------------------------------------------------------------------------------------------
- * Date & Time: 12/30/2020 (4:23 PM)
- * ------------------------------------------------------------------------------------------------------
- *
- * Copyright (c) 2020. This project is released under the standard of CodeCanyon License.
- * You may NOT modify/redistribute this copy of the project. We reserve the right to take legal actions
- * if any part of the license is violated. Learn more: https://codecanyon.net/licenses/standard.
- *
- * ------------------------------------------------------------------------------------------------------
- */
 
 namespace Modules\BitGo;
 
